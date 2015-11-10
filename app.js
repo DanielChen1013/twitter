6efe9cdc176c10b01a3cfc34867b3fec52d1cdea
Dan Chen <danielchen@Daniels-MacBook-Air.local>
2015-11-10 14:26:05 -0500

    hoping this works lol

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 // app.js will be our main application
 // Static (unchanging) files in public will be automatically served when requested
 // files in routes and views will let us define dynamic content.
-
 var express = require( 'express' );
 var app = express(); // creates an instance of an express application
+
+
+app.use(function(req, res, next){
+	console.log(req.method + " " + req.path + " " + res.statusCode); //log the middleware, order counts, must come first
+	
+	next();
+})
 
 app.get("/", function(req, res){
 	res.send("Welcome message");
 })
 
-// app.listen(3000);
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+app.get("/news", function(req, res){
+	res.send("No new is good news");
 })
+
+
+app.listen(3000, function(){
+	console.log("Serving listening on 3000")
+})
+
+// var server = app.listen(3000, function(){
+//   var host = server.address().address;
+//   var port = server.address().port;
+//   console.log('Example app listening at http://%s:%s', host, port);
+// })
